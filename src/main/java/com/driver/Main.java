08703d610da57ffe95c1665041396446b00368ac
Aswinkumar20 <aswinkumar11999@gmail.com>
2022-10-08 11:23:38 +0530

    added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly();
+//  System.out.println(obj.name = "aswin");
+//    obj.name = "aswin";
+//    java: name has private access in com.driver.RWOnly
+
+    obj.setName("aswin");
+    System.out.println(obj.getName());
+  }
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // constructor
+    public RWOnly(){}
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
 }
